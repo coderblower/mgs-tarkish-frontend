a34129a0ad1d6e6c0f8afdddebf606d5cc2d77da
saiful110816 <saiful110816@acc.edu.bd>
2025-01-08 15:41:34 +0600

    sdf

--- src/component/ProfileMenu/FullPageDocumentViewer.jsx
+++ src/component/ProfileMenu/FullPageDocumentViewer.jsx
@@ -83,7 +83,7 @@ const FullPageDocumentViewer = ({ file, onClose }) => {
       
         <button
           onClick={onClose}
-          className="absolute top-4 right-4 bg-red-500 text-white px-4 py-2 rounded"
+          className="absolute top-4 right-4 bg-red-500 text-white px-4 py-2 rounded z-10"
         >
           Close
         </button>
