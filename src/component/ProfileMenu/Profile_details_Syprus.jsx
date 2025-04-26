06b001b48fa887ee897d7324abf9bdefaaa24035
CodeBlower <saifulrmail@gmail.com>
2025-04-26 23:12:56 +0600

    ss

--- src/component/ProfileMenu/Profile_details_Syprus.jsx
+++ src/component/ProfileMenu/Profile_details_Syprus.jsx
@@ -367,7 +367,7 @@ const Profile_Details = ({userId}) => {
           <div  >
             <div className="flex justify-center lg:px-5">
             
-              <div   className="w-full">
+              <div ref={pdfRef}  className="w-full">
               
                 {/* <div>
             <button className="bg-red-600" onClick={downloadImage}>
@@ -381,7 +381,7 @@ const Profile_Details = ({userId}) => {
                  
                   <div >
                     <h2 className="font-bold mb-3 text-[20px]">Basic Info</h2>
-                    <div ref={pdfRef} className="border-b-4 border-gray-400 pb-2 lg:flex justify-between gap-5">
+                    <div  className="border-b-4 border-gray-400 pb-2 lg:flex justify-between gap-5">
                       <div  >
                         <TextTitle title="Name:" data={data?.name} />
                         <TextTitle title="Phone:" data={data?.phone} />
